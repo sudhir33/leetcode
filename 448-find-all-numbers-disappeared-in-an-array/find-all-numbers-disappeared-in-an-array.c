84e8b3cddf9aeea3036b3b12247e7cac5bb781b9
sudhir33 <32663121+sudhir33@users.noreply.github.com>
2026-07-29 14:25:18 +0530

    Time: 0 ms (100.00%) | Memory: 37.1 MB (28.22%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.c
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.c
@@ -2,21 +2,70 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* findDisappearedNumbers(int* nums, int numsSize, int* returnSize) {
-    int *res=(int*)calloc((numsSize+1),sizeof(int));
-    int *res1=(int*)calloc((numsSize+1),sizeof(int));
-    int i,j=0;
+    int *res,*countarray;
+    res=(int*)malloc(numsSize*sizeof(int));//int res[numsSize];
+    countarray=(int*)calloc(numsSize+1,sizeof(int));//int countarray[numsSize+1];
+    int i,ind;
     for(i=0;i<numsSize;i++)
     {
-        res[nums[i]]++;
+        ind=nums[i];
+        countarray[ind]+=1;
     }
-    for(i=1;i<numsSize+1;i++)
+    int j;
+    for(i=1,j=0;i<numsSize+1;i++)
     {
-        if(res[i]==0)
+        if(countarray[i]==0)
         {
-            res1[j++]=i;
+            res[j]=i;
+            j++;
         }
     }
     *returnSize=j;
-    return res1;
-
+    return res;
 }
+
+/*
+                      i
+nums=[4,3,2,7,8,2,3,1]
+numsSize=8
+
+counarray[9]
+
+0 1 2 2 1 0 0 1 1
+0 1 2 3 4 5 6 7 8
+
+
+*resize=j
+res=[5,6]
+
+
+
+
+
+
+            i
+[6,5,6,3,2,2]
+numsSize=6    1.6
+
+int countarray[numsSize+1]// countarray[7]
+
+0 0 2 1 0 1 2 
+0 1 2 3 4 5 6
+
+[1,4]
+
+
+
+
+[4,3,2,7,8,2,3,1]
+n=8 
+1 2 3 4 5 6 7 8
+[5,6]
+
+
+[1,1]
+n=2
+1 2
+
+[2]
+*/
