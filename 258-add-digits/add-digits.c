de8ce4c04beb9ee50e2aa8b65306c95a2e5386ea
sudhir33 <32663121+sudhir33@users.noreply.github.com>
2026-09-07 10:41:41 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (78.82%) - LeetSync

---
+++ 258-add-digits/add-digits.c
@@ -0,0 +1,16 @@
+int addDigits(int num) {
+    int res=0,rem;
+    while(num)
+    {
+        rem=num%10;
+        num=num/10;
+        res=res+rem;
+        if(num==0 && res>9)
+        {
+            num=res;
+            res=0;
+        }
+    }
+    return res;
+}
+
